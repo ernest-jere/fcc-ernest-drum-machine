6dbc666c2c8cefb6581af80d6444d4b91686217b
ernest-jere <ernestmmjere@outlook.com>
2025-05-14 15:34:19 +0200

    modified index.js

--- src/index.js
+++ src/index.js
@@ -116,4 +116,9 @@ class App extends React.Component {
   }
 }
 
-React.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
